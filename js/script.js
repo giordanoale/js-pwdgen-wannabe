03ce4f3efbb337d5615c2d5f18f2dff0a1b738c9
giordanoale <a.giordano89@gmail.com>
2021-02-25 17:25:49 +0100

    Aggiunti colori e formattazione

--- js/script.js
+++ js/script.js
@@ -4,6 +4,8 @@ var cognome = prompt("Qual è il tuo cognome?");
 
 var colore = prompt("Qual è il tuo colore preferito?");
 
+var danger = alert("Attendi! Stiamo crittografando il messaggio!")
+
 var password = nome + cognome + colore + 27;
 
 document.getElementById('password').innerHTML = password;
